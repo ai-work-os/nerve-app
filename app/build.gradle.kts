10fa5152efcd943a9a4e9064b739b0d0b8b18612
renjinxi <f862250480@qq.com>
2026-05-27 09:21:16 +0800

    release(android): bump versionCode 29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.nerve.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 28
-        versionName = "0.8.5"
+        versionCode = 29
+        versionName = "0.8.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
