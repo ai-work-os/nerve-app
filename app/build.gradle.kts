ec64e222eff600be7ce82e5a519982b8d3316832
renjinxi <f862250480@qq.com>
2026-05-27 22:15:59 +0800

    release(android): bump version to 0.8.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.nerve.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 35
-        versionName = "0.8.11"
+        versionCode = 36
+        versionName = "0.8.12"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
